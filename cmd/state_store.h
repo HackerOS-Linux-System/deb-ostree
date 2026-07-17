8d47ae2e55a8249724078f7aae195f1063656d5f
michal92299 <chudybyczek2137@gmail.com>
2026-07-17 08:13:24 +0200

    Update state_store.h

--- cmd/state_store.h
+++ cmd/state_store.h
@@ -8,7 +8,7 @@
  * przez wysokopoziomowe API (Config). Reszta kodu dostaje gotowy struct
  * Config i nigdy nie czyta pliku .hk bezposrednio.
  *
- * Wersja: 0.1.0
+ * Wersja: 0.2.0
  */
 
 #include "types.h"
