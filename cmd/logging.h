35f54a00c5f8cc2235d91a828f1c96c6d3e57c4b
michal92299 <chudybyczek2137@gmail.com>
2026-07-17 08:10:07 +0200

    Update logging.h

--- cmd/logging.h
+++ cmd/logging.h
@@ -1,19 +1,24 @@
 #pragma once
 /*
  * deb-ostree -- logging.h
- * Prosty, kolorowy logger ANSI z poziomami DEBUG/INFO/WARN/ERROR.
- * Wyjscie kolorowe tylko gdy stdout/stderr jest TTY (np. nie do pliku/pipe).
+ * Kolorowy logger ANSI z poziomami DEBUG/INFO/WARN/ERROR.
+ * Obsługuje opcjonalny log do pliku (--log-file) (#12).
  *
- * Wersja: 0.1.0
+ * Wersja: 0.2.0
  */
 
 #include <string_view>
+#include <string>
 
 namespace debostree::log {
 
 enum class Level { Debug, Info, Warn, Error };
 
 void set_verbose(bool verbose);
+/* Ustawia plik logu (np. /var/log/deb-ostree.log).
+ * Pusta ścieżka = wyłącz logowanie do pliku.
+ * Plik jest otwierany w trybie append. (#12) */
+void set_log_file(const std::string& path);
 void emit(Level level, std::string_view msg);
 
 inline void debug(std::string_view m) { emit(Level::Debug, m); }
